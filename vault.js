bb5857c0a1e25bb0d9428da47c4ce89a95658670
Renee <rchar20@gmail.com>
2016-09-08 18:46:53 -1000

    Created a secret Vault object and made public functions of setValue and getValue

--- vault.js
+++ vault.js
@@ -1,4 +1,16 @@
 'use strict';
 module.exports = function() {
-  
+  var theSecretVault = {};
+  return {
+  	setValue: function(key,value) {
+  		theSecretVault[key] = value; 
+  	},
+  	getValue: function (key) {
+  		if (!theSecretVault[key]) {
+  			return null;
+  		} else {
+	  		return theSecretVault[key];
+  		}
+  	}
+  }
 };
